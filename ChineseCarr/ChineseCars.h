5c83359456c1db32a1530d1754857d72aa989ad9
Egor <gorfirstov@mail.ru>
2025-04-27 19:40:30 +0500

    Добавьте файлы проекта.

---
+++ ChineseCarr/ChineseCars.h
@@ -0,0 +1,153 @@
+#ifndef CHINESE_CARS_H
+#define CHINESE_CARS_H
+
+// Шаблонный класс реализующий Итератор
+template<typename T>
+class Iterator
+{
+protected:
+    Iterator() {}
+
+public:
+    virtual ~Iterator() {}
+    virtual void First() = 0;
+    virtual void Next() = 0;
+    virtual bool IsDone() const = 0;
+    virtual T GetCurrent() const = 0;
+};
+
+
+template<typename T>
+class StackIterator : public Iterator<T>
+{
+private:
+    T* StackContainer;
+    size_t Pos;
+    size_t Size;
+
+public:
+    StackIterator(T* container, size_t size)
+        : StackContainer(container), Pos(0), Size(size) {
+    }
+
+    void First() override { Pos = 0; }
+    void Next() override { Pos++; }
+    bool IsDone() const override { return (Pos >= Size); }
+    T GetCurrent() const override { return StackContainer[Pos]; }
+};
+
+// Шаблонный класс Стек
+const size_t MaxSize = 100;
+
+template <typename T>
+class Stack_class
+{
+private:
+    T Items[MaxSize];
+    size_t Top;
+
+public:
+    bool Is_empty() const { return (Top == 0); }
+    size_t Size() const { return Top; }
+    void Push(T newObject) { Items[Top++] = newObject; }
+    T Pop() { return Items[--Top]; }
+    T Get_element_index(size_t index) const { return Items[index]; }
+
+    Stack_class() : Top(0) {}
+
+    // Получение итератора для обхода контейнера
+    class Iterator<T>* GetIterator()
+    {
+        return new StackIterator<T>(Items, Top);
+    };
+};
+
+// Итератор для обхода массива
+
+template<typename T>
+class ArrayIterator : public Iterator<T>
+{
+private:
+
+    const T* ArrayContainer;
+    size_t Pos;
+    size_t Size;
+
+public:
+    ArrayIterator(T* container, size_t size)
+        : ArrayContainer(container), Pos(0), Size(size) {
+    }
+
+    void First() override { Pos = 0; }
+    void Next() override { Pos++; }
+    bool IsDone() const override { return (Pos >= Size); }
+    T GetCurrent() const override { return ArrayContainer[Pos]; }
+};
+
+
+// Шаблонный класс Массив
+template <typename T>
+class ArrayClass
+{
+private:
+
+    T Items[MaxSize];
+    size_t ArraySize;
+
+public:
+
+    void Add(T newObject) { Items[ArraySize++] = newObject; }
+    size_t Size() const { return ArraySize; }
+    T GetElement(size_t index) const { return Items[index]; }
+
+    // Получение итератора для обхода контейнера
+    class Iterator<T>* GetIterator()
+    {
+        return new ArrayIterator<T>(Items, ArraySize);
+    };
+
+    ArrayClass() : ArraySize(0) {}
+};
+
+// Декоратор для итератора (пустой)
+
+template<typename T>
+class IteratorDecorator : public Iterator<T>
+{
+protected:
+    Iterator<T>* It;
+
+public:
+    IteratorDecorator(Iterator<T>* it) : It(it) {}
+    virtual ~IteratorDecorator() { delete It; }
+    virtual void First() { It->First(); }
+    virtual void Next() { It->Next(); }
+    virtual bool IsDone() const { return It->IsDone(); }
+    virtual T GetCurrent() const { return It->GetCurrent(); }
+};
+
+// Адаптер для контейнерных классов STL
+
+template<typename ContainerType, typename ItemType>
+class ConstIteratorAdapter : public Iterator<ItemType>
+{
+protected:
+    ContainerType* Container;
+    typename ContainerType::const_iterator It;
+
+public:
+    ConstIteratorAdapter(ContainerType* container)
+        : Container(container)
+    {
+        It = Container->begin();
+    }
+
+    virtual ~ConstIteratorAdapter() {}
+    virtual void First() { It = Container->begin(); }
+    virtual void Next() { It++; }
+    virtual bool IsDone() const { return (It == Container->end()); }
+    virtual ItemType GetCurrent() const { return *It; }
+};
+
+
+#endif // CHINESE_CARS_H
---
+++ ChineseCarr/main.cpp
@@ -0,0 +1,260 @@
+﻿#include <iostream>
+#include <list>
+#include "ChineseCars.h"
+
+using namespace std;
+
+// Мощность автомобиля
+enum class Car_power : int
+{
+    Low,
+    Average,
+    High,
+    Unknown
+};
+
+class Car {
+private:
+    bool electric;
+    Car_power Power;
+
+protected:
+    string brand;
+    string fuel_type;
+    string OS;
+
+    bool subscription;
+
+public:
+    Car(Car_power power) : Power(power), subscription(false) {
+        subscription = static_cast<bool>(rand() % 2);
+    };
+    virtual ~Car();
+
+    string get_brand() const { return brand; }
+    bool isElectric() const { return electric; }
+    string getOS() const { return OS; }
+
+    bool IsSubscription() const { return subscription; }
+
+    Car_power GetPower() const { return Power; }
+
+    virtual void drive() {
+        if (IsSubscription()) {
+            cout << "Запускаю автомобиль " << brand << endl;
+        }
+        else {
+            cout << "Нет оплаты! Невозможно запустить " << brand << endl;
+        }
+    }
+};
+
+Car::~Car() {}
+
+class BYD : public Car {
+public:
+    BYD();
+    ~BYD();
+
+    string get_brand() const;
+
+    void drive();
+};
+
+BYD::BYD() : Car(Car_power::High) {
+    brand = "BYD";
+    fuel_type = "Электрический";
+}
+
+BYD::~BYD() {}
+
+string BYD::get_brand() const { return Car::get_brand(); }
+
+void BYD::drive() {
+    Car::drive();
+    cout << "Автомобиль " << brand << " на ходу!" << endl;
+}
+
+class Geely : public Car {
+public:
+    Geely();
+    ~Geely();
+
+    void drive();
+};
+
+Geely::Geely() : Car(Car_power::Low) {
+    brand = "Geely";
+    fuel_type = "Бензин";
+}
+
+Geely::~Geely() {}
+
+void Geely::drive() {
+    Car::drive();
+    cout << "Автомобиль " << brand << " на ходу!" << endl;
+}
+
+class NIO : public Car {
+public:
+    NIO();
+    ~NIO();
+
+    void drive();
+};
+
+NIO::NIO() : Car(Car_power::Average) {
+    brand = "NIO";
+    fuel_type = "Электрический";
+}
+
+NIO::~NIO() {}
+
+void NIO::drive() {
+    Car::drive();
+    cout << "Автомобиль " << brand << " на ходу!" << endl;
+}
+
+// Фабричный метод
+
+enum class Car_type : int
+{
+    BYD = 1,
+    Geely = 2,
+    NIO = 3,
+
+    Undefined = 0
+};
+
+Car* Create_car(Car_type type) {
+    Car* new_car = nullptr;
+
+    if (type == Car_type::BYD) {
+        new_car = new BYD;
+    }
+    else if (type == Car_type::Geely) {
+        new_car = new Geely;
+    }
+    else if (type == Car_type::NIO) {
+        new_car = new NIO;
+    }
+
+    return new_car;
+}
+
+// Декоратор итератора для выделения автомобилей по мощности
+
+class PowerDecorator : public IteratorDecorator<class Car*> {
+private:
+    Car_power TargetPower;
+
+public:
+    PowerDecorator(Iterator<Car*>* it, Car_power power)
+        : IteratorDecorator<Car*>(it), TargetPower(power) {}
+
+    void First() {
+        It->First();
+        while (!It->IsDone() && It->GetCurrent()->GetPower() != TargetPower) {
+            It->Next();
+        }
+    }
+
+    void Next() {
+        do {
+            It->Next();
+        } while (!It->IsDone() && It->GetCurrent()->GetPower() != TargetPower);
+    }
+};
+
+// Декоратор итератора для выделения автомобилей по наличию оплаты
+
+class SubscriptionDecorator : public IteratorDecorator<class Car*> {
+private:
+    bool TargetSubscription;
+
+public:
+    SubscriptionDecorator(Iterator<Car*>* it, bool isSubscription)
+        : IteratorDecorator<Car*>(it), TargetSubscription(isSubscription) {}
+
+    void First() {
+        It->First();
+        while (!It->IsDone() && It->GetCurrent()->IsSubscription() != TargetSubscription) {
+            It->Next();
+        }
+    }
+
+    void Next() {
+        do {
+            It->Next();
+        } while (!It->IsDone() && It->GetCurrent()->IsSubscription() != TargetSubscription);
+    }
+};
+
+void Drive_all(Iterator<Car*>* it) {
+    for (it->First(); !it->IsDone(); it->Next()) {
+        Car* currentCar = it->GetCurrent();
+        currentCar->drive();
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    size_t N = 30;
+
+
+    ArrayClass<Car*> carArray;
+    for (size_t i = 0; i < N; i++) {
+        int car_num = rand() % 3 + 1;
+        Car_type car_type = static_cast<Car_type>(car_num);
+        Car* newCar = Create_car(car_type);
+        carArray.Add(newCar);
+    }
+
+    wcout << L"Размер массива автомобилей: " << carArray.Size() << endl;
+
+    list<Car*> carList;
+    for (size_t i = 0; i < N; i++) {
+        int car_num = rand() % 3 + 1;
+        Car_type car_type = static_cast<Car_type>(car_num);
+        Car* newCar = Create_car(car_type);
+        carList.push_back(newCar);
+    }
+
+    wcout << L"Размер списка автомобилей: " << carList.size() << endl;
+
+    // Обход всех автомобилей
+    cout << endl << "Попробовать запустить все автомобили: " << endl;
+    Iterator<Car*>* allIt = carArray.GetIterator();
+    Drive_all(allIt);
+    delete allIt;
+
+    // Обход всех автомобилей с оплатой каршеринга
+    cout << endl << "Запустить только если есть оплата каршеринга:" << endl;
+    Iterator<Car*>* goodIt = new SubscriptionDecorator(carArray.GetIterator(), true);
+    Drive_all(goodIt);
+    delete goodIt;
+
+    // Обход всех мощных автомобилей
+    cout << endl << "Все мощные автомобили:" << endl;
+    Iterator<Car*>* powerIt = new PowerDecorator(carArray.GetIterator(), Car_power::High);
+    Drive_all(powerIt);
+    delete powerIt;
+
+    // Обход всех мощных автомобилей с оплатой каршеринга
+    cout << endl << "Все мощные автомобили с оплатой каршеринга:" << endl;
+    Iterator<Car*>* goodPowerIt =
+        new SubscriptionDecorator(new PowerDecorator(carArray.GetIterator(), Car_power::High), true);
+    Drive_all(goodPowerIt);
+    delete goodPowerIt;
+
+    // Демонстрация работы адаптера
+    cout << endl << "Все мощные автомобили с оплатой (другой контейнер):" << endl;
+    Iterator<Car*>* adaptedIt = new ConstIteratorAdapter<std::list<Car*>, Car*>(&carList);
+    Iterator<Car*>* adaptedPowerIt = new SubscriptionDecorator(new PowerDecorator(adaptedIt, Car_power::High), true);
+    Drive_all(adaptedPowerIt);
+    delete adaptedPowerIt;
+
+    return 0;
+}
+
